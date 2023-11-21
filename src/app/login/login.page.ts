76d246582e6fbbac7e3c868b52c148d0fbf892c8
Will Young <williamrayyoung@gmail.com>
2023-11-21 18:14:03 -0500

    added login in form page to JSON object for backend connection

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule]
 })
 export class LoginPage implements OnInit {
+  constructor(
 
-  constructor() { }
+  ) { }
+
+  loginForm = new FormGroup({
+    emailid: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   ngOnInit() {
     
+  }
+  SaveData(){
+    console.log(this.loginForm.value)
   }
 
 }
