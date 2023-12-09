11f143e08e7bee72f1673c52bdfdd85d7079a34c
Sanjana_Dhiman <dhiman96sanjana@gmail.com>
2023-12-09 13:48:49 -0500

    meal-detail changes

--- src/app/meal-detail/meal-detail.page.ts
+++ src/app/meal-detail/meal-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-meal-detail',
@@ -10,11 +11,26 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class MealDetailPage implements OnInit {
+export class MealDetailPage {
 
-  constructor() { }
+  meal: {
+    pictureUrl: string;
+    name: string;
+    fats: number;
+    proteins: number;
+    calories: number;
+    carbs: number;
+  };
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
+    // Dummy data for demonstration
+    this.meal = {
+      pictureUrl: 'assets/icon/breakfast_food.jpg',
+      name: "Lunch",
+      fats: 10,
+      proteins: 20,
+      calories: 300,
+      carbs: 30,
+    };
   }
-
 }
