e059d0c806f7062f0e41a94a87567194c1c31c40
Nishad Vyas <59637864+nishadvyas99@users.noreply.github.com>
2023-12-09 21:40:43 -0500

    Connected meal-deal and scan with Backend

--- src/app/meal-detail/meal-detail.page.ts
+++ src/app/meal-detail/meal-detail.page.ts
@@ -3,34 +3,70 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
+import { routes } from '../app.routes';
+
+
+interface Meal {
+  calories: number;
+  carbs: number;
+  description: string;
+  fat: number;
+  image_url: string;
+  meal_date: string;
+  proteins: number;
+  title: string;
+  user: string;
+  user_id: string;
+  weight: number;
+}
+
 
 @Component({
   selector: 'app-meal-detail',
   templateUrl: './meal-detail.page.html',
   styleUrls: ['./meal-detail.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, HttpClientModule]
 })
+
 export class MealDetailPage {
 
-  meal: {
-    pictureUrl: string;
-    name: string;
-    fats: number;
-    proteins: number;
-    calories: number;
-    carbs: number;
-  };
+  meal: Meal;
 
-  constructor(private route: ActivatedRoute) {
+  // meal: {
+  //   pictureUrl: string;
+  //   name: string;
+  //   fats: number;
+  //   proteins: number;
+  //   calories: number;
+  //   carbs: number;
+  // };
+
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient,
+    private navCtrl: NavController) {}
+
     // Dummy data for demonstration
-    this.meal = {
-      pictureUrl: 'assets/icon/breakfast_food.jpg',
-      name: "Lunch",
-      fats: 10,
-      proteins: 20,
-      calories: 300,
-      carbs: 30,
-    };
-  }
+    // this.meal = {
+    //   pictureUrl: 'assets/icon/breakfast_food.jpg',
+    //   name: "Lunch",
+    //   fats: 10,
+    //   proteins: 20,
+    //   calories: 300,
+    //   carbs: 30,
+    // };
+
+    ngOnInit() {
+      const navigation = this.router.getCurrentNavigation();
+      if (navigation?.extras?.state?.['meal']) {
+        this.meal = navigation.extras.state['meal'] as Meal;
+      }
+    }
+
+    goToDashboard() {
+      this.navCtrl.navigateRoot('/dashboard');
+    }
+  
 }
--- src/app/meal-scan/meal-scan.page.ts
+++ src/app/meal-scan/meal-scan.page.ts
@@ -5,6 +5,9 @@ import { IonicModule } from '@ionic/angular';
 import { AzureBlobStorageService } from '../azure-blob-storage.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { delay } from 'rxjs/operators';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-meal-scan',
@@ -20,10 +23,14 @@ export class MealScanPage implements OnInit {
   imageFileName: string = "no-preview.png";
   imageUrl: string;
   imageUrlDefault: string = "https://caloriecompass.blob.core.windows.net/meals/no-preview.png";
+  title: string;
+  mealDate: string;
+  description: string;
 
   sas = "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-05-31T11:45:28Z&st=2023-12-08T04:45:28Z&spr=https&sig=WkEZIc3AxQsnCAhaaeOcgyrfctNu8tCVyeKbgZbGH8E%3D"
 
-  constructor(private blobService: AzureBlobStorageService) { }
+  constructor(private blobService: AzureBlobStorageService, private http: HttpClient,
+    private navCtrl: NavController, private router: Router) { }
 
   submitForm() {
     // You can perform actions with the form data here, such as sending it to a server
@@ -33,6 +40,22 @@ export class MealScanPage implements OnInit {
     console.log('File Name:', this.imageFileName);
     this.imageUrl = 'https://caloriecompass.blob.core.windows.net/meals/'+this.imageFileName;
     this.displayImage = true;
+
+    const mealData = {
+      title: this.title,
+      image_url: this.imageUrl,
+      meal_date: this.mealDate,
+      description: this.description
+    };
+    console.log('Sending meal data:', mealData);
+    this.http.post('http://127.0.0.1:5000/auth/add_meal', mealData,{ withCredentials: true })
+        .subscribe(response => {
+            console.log('Meal data submitted:', response);
+            this.router.navigate(['/meal-detail'], { state: { meal: response } });
+        }, error => {
+            console.error('Error submitting meal data:', error);
+        });
+
     
     
     // Simulate an asynchronous operation (e.g., API call) with a delay to give time to azure to load
